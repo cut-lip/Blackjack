12cfbe71f351f3937769d8e5140ada51dd60b374
Nicholas Cutlip <cutlipn@gmail.com>
2022-10-27 19:01:35 -0700

    Seperate deck class declarations and defintions into header and cpp files

--- Assignment5/deck.cpp
+++ Assignment5/deck.cpp
@@ -5,3 +5,36 @@
 	deck.cpp
 	Cpp file for the definitions of the deck class.
 */
+
+#include "deck.h"
+
+
+deck::deck() {
+	// Initialize empty vector with 54 elements
+		// Populate vector with cards of each suit and value
+	for (cards::cardValue val : cards::allVals) {
+		for (cards::cardSuit suit : cards::allSuits) {
+			deckVec.push_back(card::card(val, suit));
+		}
+	}
+}
+
+// Return a random card from the deck (removes card from deck)
+card deck::dealCard() {
+	// Provide random number generator seed based off clock
+	srand(static_cast<unsigned int> (time(0)));
+
+		// Randomly select an index from the deck, and retrieve the card from that index
+	int index = (rand() % deckVec.size());
+	card selection = deckVec.at((size_t)index);
+
+		// Remove selected card from the deck, and return selected card
+	deck::deckVec.erase(deckVec.begin() + index);
+	return selection;
+}
+
+// Returns the current size of the deck.
+int deck::deckSize() {
+	return (int)deckVec.size();
+}
+
--- Assignment5/deck.h
+++ Assignment5/deck.h
@@ -16,35 +16,13 @@
 class deck {
 public:
 	// deck constructor
-	deck() {
-		// Initialize empty vector with 54 elements
+	deck();
 
-		// Populate vector with cards of each suit and value
-		for (cards::cardValue val : cards::allVals) {
-			for (cards::cardSuit suit : cards::allSuits) {
-				deckVec.push_back(card::card(val, suit));
-			}
-		}
-	}
-	
 	// Return a random card from the deck (removes card from deck)
-	card dealCard() {
-		// Provide random number generator seed based off clock
-		srand(static_cast<unsigned int> (time(0)));
-
-		// Randomly select an index from the deck, and retrieve the card from that index
-		int index = (rand() % deckVec.size());
-		card selection = deckVec.at((size_t)index);
-
-		// Remove selected card from the deck, and return selected card
-		deckVec.erase(deckVec.begin() + index);
-		return selection;
-	}
+	card dealCard();
 
 	// Returns the current size of the deck.
-	int deckSize() {
-		return (int)deckVec.size();
-	}
+	int deckSize();
 
 private:
 	// Vector of card objects to hold the cards in the deck
